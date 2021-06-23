8ea3e99b1dafae8a70c7692845d59301982537e9
Jared Ramsey <ramsey_jared@bah.com>
2021-06-23 14:21:49 -0400

    debugging

--- backend/start.sh
+++ backend/start.sh
@@ -6,4 +6,4 @@ sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db
 psql -U postgres -h postgres -d game_changer -f /usr/src/app/node_app/init/create_clones.sql && \
 psql -U postgres -h postgres -d game_changer -f /usr/src/app/node_app/init/create_admins.sql && \
 psql -U postgres -h postgres -d game_changer -f /usr/src/app/node_app/init/pop_mini_RE.sql && \
-nodemon index.js
+sleep 60000
